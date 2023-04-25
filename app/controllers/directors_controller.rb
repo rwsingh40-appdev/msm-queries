823df9f33513be420e6015add3cce56ca44c2bf4
Raj Singh <rsingh40@chicagobooth.edu>
2023-04-25 00:17:17 +0000

    details page drafted

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,12 +2,12 @@ class DirectorsController < ApplicationController
 
   def index
     @list_of_directors = Director.all
-    
-
     render({:template => "directors_templates/index.html.erb"})
   end
 
   def director_details
+    id = params.fetch("director_id")
+    @target_director = Director.where({:id => id}).first
     render({:template => "directors_templates/details.html.erb"})
   end
 
@@ -15,9 +15,20 @@ class DirectorsController < ApplicationController
     array = Director.all
     eldest = array.where({:dob => array.minimum(:dob)})
     eldest = eldest[0]
+    @eldest_id = eldest.id
     @eldest_name = eldest.name
-    @eldest_dob = eldest.dob
+    @eldest_dob = eldest.dob.strftime("%B %e, %Y")
     render({:template => "directors_templates/eldest.html.erb"})
+  end
+  
+  def director_youngest
+    array = Director.all
+    youngest = array.where({:dob => array.maximum(:dob)})
+    youngest = youngest[0]
+    @youngest_id = youngest.id
+    @youngest_name = youngest.name
+    @youngest_dob = youngest.dob.strftime("%B %e, %Y")
+    render({:template => "directors_templates/youngest.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get("/directors", {:controller => "directors", :action => "index" })
   get("/directors/eldest", {:controller => "directors", :action => "director_eldest"})
+  get("/directors/youngest", {:controller => "directors", :action => "director_youngest"})
   get("/directors/:director_id", {:controller => "directors", :action => "director_details"})
 
 
